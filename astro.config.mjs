f30bef229e1bca24d6000682b7a7f952ca653817
Aditya Kumar <117935160+AS1100K@users.noreply.github.com>
2024-11-01 16:00:48 +0530

    Added code highlight support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,37 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      theme: "github-light",
+      // Alternatively, provide multiple themes
+      // See note below for using dual light/dark themes
+      //   themes: {
+      //     light: "github-light",
+      //     dark: "github-dark",
+      //   },
+      // Disable the default colors
+      // https://shiki.style/guide/dual-themes#without-default-color
+      // (Added in v4.12.0)
+      defaultColor: false,
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Add custom aliases for languages
+      // Map an alias to a Shiki language ID: https://shiki.style/languages#bundled-languages
+      // https://shiki.style/guide/load-lang#custom-language-aliases
+      langAlias: {
+        cjs: "javascript",
+      },
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
+});
