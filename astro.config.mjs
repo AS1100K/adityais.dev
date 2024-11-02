fc93ad83e9f13c5366379ce0ee64d4060aa795b3
Aditya Kumar <117935160+AS1100K@users.noreply.github.com>
2024-11-02 13:07:52 +0530

    Added SSR for featured projects component with caching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
@@ -34,4 +36,13 @@ export default defineConfig({
       transformers: [],
     },
   },
+
+  output: "server",
+  adapter: netlify({
+    imageCDN: true,
+    cacheOnDemandPages: true,
+    experimental: {
+      serverIsland: true,
+    },
+  }),
 });
