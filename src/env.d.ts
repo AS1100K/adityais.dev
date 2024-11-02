7dbe5895fd53c7ec3e3e2bb2cce5db6273c367f4
Aditya Kumar <117935160+AS1100K@users.noreply.github.com>
2024-11-02 16:28:51 +0530

    Using `env` variables as specified by cloudflare workers

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+
+type Env = {
+  GITHUB_API_KEY: string;
+};
+
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+
+declare namespace App {
+  interface Locals extends Runtime {}
+}
