f10c1045440c35a2d809e0f33f307f4df16ca1f6
Aditya Kumar <git@adityais.dev>
2024-12-30 14:50:40 +0530

    Migrate to tailwindcss (#2)
    * Added Tailwind Css
    
    * Migrated to Tailwind CSS
    
    * Updated lock
    
    * Migrated `Header.astro` to tailwind
    
    * Migrated `Button.astro`
    
    * Migrated `FeaturedProject.astro`
    
    * Fixed `index.astro`

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      borderWidth: {
+        '1': '1px'
+      },
+      width: {
+        featured_project: 'calc(100% - 2rem)'
+      },
+      colors: {
+        primary: "#56ba63",
+        secondary: "#9cdda4",
+        accent: "#6fd67d",
+        background: "#f9fcf9",
+        text: "#09100a",
+      },
+    },
+  },
+  plugins: [],
+};
