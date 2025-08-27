fee83b50e480345853e5a071bcafca886642fa54
Aditya Kumar <git@adityais.dev>
2025-08-27 21:03:21 +0530

    Add support for paginated article fetching from Dev.to API

--- tools/dev-to-sync/main.py
+++ tools/dev-to-sync/main.py
@@ -5,39 +5,57 @@ import utils
 API_KEY = os.getenv("DEV_TO_API_KEY")
 AUTH_HEADER = {"api-key": API_KEY}
 
-# TODO: Support for multiple pages
-articles = requests.get(
-    "https://dev.to/api/articles/me/all",
-    headers=AUTH_HEADER
-).json()
+page_number = 1
 
-for article in articles:
-    if article["user"]["username"].lower() != "as1100k":
-        continue
-
-    is_published = article["published"]
-
-    file_path = (
-        utils.file_path_from_canonical_url(article["canonical_url"], is_published)
-        if article["canonical_url"] is not None and article["canonical_url"] != article["url"]
-        # TODO: Make sure we don't overwrite anything
-        else utils.generate_file_path_from_title(article["title"], is_published)
+while True:
+    articles = requests.get(
+        "https://dev.to/api/articles/me/all",
+        headers=AUTH_HEADER,
+        params={
+            "page": page_number,
+            "per_page": 30
+        }
     )
 
-    print(f"Generated: {file_path}")
+    if articles.status_code != 200:
+        print(f"Got Status Code: {articles.status_code}")
+        break
 
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    with open(file_path, "w") as f:
-        f.write(f"""---
-layout: "@layout/blog.astro"
-title: "{article["title"]}"
-description: "{article["description"]}"
-pubDate: {article["published_at"] if is_published else "2025-08-27"}
-v1Data:
-    devtoUrl: "{article["url"]}"
-    readingTimeMinutes: {article["reading_time_minutes"]}
-    {f"coverImage: {article['cover_image']}" if article['cover_image'] is not None else "" }
----
+    articles = articles.json()
 
-{article["body_markdown"]}
-            """)
+
+    if len(articles) == 0:
+        break
+
+    for article in articles:
+        if article["user"]["username"].lower() != "as1100k":
+            continue
+
+        is_published = article["published"]
+
+        file_path = (
+            utils.file_path_from_canonical_url(article["canonical_url"], is_published)
+            if article["canonical_url"] is not None and article["canonical_url"] != article["url"]
+            # TODO: Make sure we don't overwrite anything
+            else utils.generate_file_path_from_title(article["title"], is_published)
+        )
+
+        print(f"Generated: {file_path}")
+
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as f:
+            f.write(f"""---
+    layout: "@layout/blog.astro"
+    title: "{article["title"]}"
+    description: "{article["description"]}"
+    pubDate: {article["published_at"] if is_published else "2025-08-27"}
+    v1Data:
+        devtoUrl: "{article["url"]}"
+        readingTimeMinutes: {article["reading_time_minutes"]}
+        {f"coverImage: {article['cover_image']}" if article['cover_image'] is not None else "" }
+    ---
+
+    {article["body_markdown"]}
+                """)
+
+    page_number += 1
